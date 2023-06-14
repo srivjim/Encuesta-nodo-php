5261d3e9720f9ad42fef3806c69e00ea272a10fa
srivjim <114186983+srivjim@users.noreply.github.com>
2023-06-14 17:14:31 +0200

    Update statistics.php

--- src/statistics.php
+++ src/statistics.php
@@ -89,6 +89,13 @@ $conn->close();
             border-radius: 5px;
         }
 
+        .chart-bar-color {
+            width: 20px;
+            height: 20px;
+            border-radius: 5px;
+            margin-left: 10px;
+        }
+
         .btn-container {
             display: flex;
             justify-content: center;
@@ -133,51 +140,72 @@ $conn->close();
                 <div class="chart-bar-value"><?php echo $statistics['Avanzar'] ?? 0; ?></div>
                 <div class="chart-bar-color" style="background-color: #f39c12;"></div>
             </div>
+            <div class="chart-bar">
+                <div class="chart-bar-label">PSOE</div>
+                <div class="chart-bar-value"><?php echo $statistics['PSOE'] ?? 0; ?></div>
+                <div class="chart-bar-color" style="background-color: #9c27b0;"></div>
+            </div>
+            <div class="chart-bar">
+                <div class="chart-bar-label">PP</div>
+                <div class="chart-bar-value"><?php echo $statistics['PP'] ?? 0; ?></div>
+                <div class="chart-bar-color" style="background-color: #3f51b5;"></div>
+            </div>
+            <div class="chart-bar">
+                <div class="chart-bar-label">VOX</div>
+                <div class="chart-bar-value"><?php echo $statistics['VOX'] ?? 0; ?></div>
+                <div class="chart-bar-color" style="background-color: #2196f3;"></div>
+            </div>
+            <div class="chart-bar">
+                <div class="chart-bar-label">Otros/No sabe</div>
+                <div class="chart-bar-value"><?php echo $statistics['Otros/No sabe'] ?? 0; ?></div>
+                <div class="chart-bar-color" style="background-color: #607d8b;"></div>
+            </div>
         </div>
 
-        <script>
-            // Obtener los datos de PHP
-            var statistics = <?php echo json_encode($statistics); ?>;
-
-            // Convertir los datos a formato adecuado para Chart.js
-            var labels = Object.keys(statistics);
-            var values = Object.values(statistics);
-
-            // Definir los colores para cada opción
-            var colors = ['#3498db', '#e74c3c', '#f39c12'];
-
-            // Configuración de la gráfica
-            var ctx = document.getElementById('chart').getContext('2d');
-            var chart = new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: labels,
-                    datasets: [{
-                        label: 'Votos',
-                        data: values,
-                        backgroundColor: colors
-                    }]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true,
-                            precision: 0
-                        }
-                    },
-                    plugins: {
-                        legend: {
-                            display: false
-                        }
-                    }
-                }
-            });
-        </script>
+        <canvas id="chart"></canvas>
 
         <div class="btn-container">
             <a class="btn" href="index.php">Volver a la Encuesta</a>
         </div>
     </div>
+
+    <script>
+        // Obtener los datos de PHP
+        var statistics = <?php echo json_encode($statistics); ?>;
+
+        // Convertir los datos a formato adecuado para Chart.js
+        var labels = Object.keys(statistics);
+        var values = Object.values(statistics);
+
+        // Definir los colores para cada opción
+        var colors = ['#3498db', '#e74c3c', '#f39c12', '#9c27b0', '#3f51b5', '#2196f3', '#607d8b'];
+
+        // Configuración de la gráfica
+        var ctx = document.getElementById('chart').getContext('2d');
+        var chart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Votos',
+                    data: values,
+                    backgroundColor: colors
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true,
+                        precision: 0
+                    }
+                },
+                plugins: {
+                    legend: {
+                        display: false
+                    }
+                }
+            }
+        });
+    </script>
 </body>
 </html>
-
