65cf8f232e00f1b2105fc3430d491fbafc0546a7
srivjim <114186983+srivjim@users.noreply.github.com>
2023-06-14 22:04:40 +0200

    Update statistics.php

--- src/statistics.php
+++ src/statistics.php
@@ -1,124 +1,3 @@
-<?php
-$servername = "192.168.58.151";
-$username = "root";
-$password = "bolson";
-$dbname = "encuesta";
-
-// Conexión a la base de datos
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificar la conexión
-if ($conn->connect_error) {
-    die("Error en la conexión a la base de datos: " . $conn->connect_error);
-}
-
-// Obtener los datos de votos por opción
-$sql = "SELECT opcion, COUNT(*) as total FROM encuesta GROUP BY opcion";
-$result = $conn->query($sql);
-
-$statistics = array();
-while ($row = $result->fetch_assoc()) {
-    $statistics[$row['opcion']] = $row['total'];
-}
-
-$conn->close();
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Estadísticas de la Encuesta</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background: linear-gradient(to right, #f9a825, #e53935);
-            color: #ffffff;
-        }
-
-        .container {
-            max-width: 800px;
-            margin: 0 auto;
-            padding: 40px;
-            background-color: rgba(0, 0, 0, 0.5);
-            border-radius: 10px;
-            box-shadow: 0 0 20px rgba(0, 0, 0, 0.3);
-            text-align: center;
-        }
-
-        h1 {
-            color: #ffffff;
-            font-size: 48px;
-            margin-bottom: 30px;
-        }
-
-        .chart-container {
-            width: 100%;
-            max-width: 600px;
-            margin: 0 auto;
-            background-color: rgba(255, 255, 255, 0.1);
-            border-radius: 10px;
-            padding: 20px;
-            margin-bottom: 40px;
-        }
-
-        .chart-title {
-            color: #ffffff;
-            font-size: 24px;
-            font-weight: bold;
-            margin-bottom: 20px;
-        }
-
-        .chart-bar {
-            display: flex;
-            align-items: center;
-            margin-bottom: 10px;
-        }
-
-        .chart-bar-label {
-            flex: 0 0 150px;
-            text-align: right;
-            padding-right: 10px;
-            color: #ffffff;
-        }
-
-        .chart-bar-value {
-            flex: 1;
-            color: #ffffff;
-            height: 24px;
-            line-height: 24px;
-            border-radius: 5px;
-        }
-
-        .chart-bar-color {
-            width: 16px;
-            height: 16px;
-            border-radius: 5px;
-            margin-left: 10px;
-        }
-
-        .btn-container {
-            display: flex;
-            justify-content: center;
-        }
-
-        .btn {
-            display: inline-block;
-            padding: 16px 32px;
-            background-color: #e53935;
-            color: #ffffff;
-            text-decoration: none;
-            font-size: 20px;
-            border-radius: 5px;
-            transition: background-color 0.3s ease;
-            margin: 0 10px;
-        }
-
-        .btn:hover {
-            background-color: #c62828;
-        }
-    </style>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-</head>
 <body>
     <div class="container">
         <h1>Estadísticas de la Encuesta</h1>
@@ -173,7 +52,19 @@ $conn->close();
         var values = Object.values(statistics);
 
         // Definir los colores para cada opción
-        var colors = ['#3498db', '#e74c3c', '#f39c12', '#9c27b0', '#3f51b5', '#2196f3', '#607d8b'];
+        var colors = {
+            'Unidas Podemos': '#8e44ad',
+            'Sumar': '#e91e63',
+            'PSOE': '#ff0000',
+            'PP': '#3f51b5',
+            'VOX': '#4caf50',
+            'Otros/No sabe': '#607d8b'
+        };
+
+        // Obtener los colores en el mismo orden que las etiquetas
+        var barColors = labels.map(function(label) {
+            return colors[label];
+        });
 
         // Configuración de la gráfica
         var ctx = document.getElementById('chart').getContext('2d');
@@ -184,7 +75,7 @@ $conn->close();
                 datasets: [{
                     label: 'Votos',
                     data: values,
-                    backgroundColor: colors
+                    backgroundColor: barColors
                 }]
             },
             options: {
